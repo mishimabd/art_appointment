b44f67b28d08dd51da9393171cedfe334043145b
mishimabd <meirkhanabdullayev@yahoo.com>
2024-09-11 01:44:34 +0500

    added a lot

--- contacts.py
+++ contacts.py
@@ -9,3 +9,13 @@ async def our_contacts(update: Update, context) -> None:
         "Мы всегда готовы помочь вам с любыми вопросами или предложениями! 😊"
     )
     await update.message.reply_text(our_contacts, parse_mode="HTML")
+
+
+async def how_to_use(update: Update, context) -> None:
+    our_contacts = (
+        "✨ Вот наши контактные данные:\n\n"
+        "📞 <b>Телефон:</b> +7 (123) 456-78-90\n"
+        "✉️ <b>Email:</b> <a href='mailto:support@talant.com'>support@talant.com</a>\n\n"
+        "Мы всегда готовы помочь вам с любыми вопросами или предложениями! 😊"
+    )
+    await update.message.reply_text(our_contacts, parse_mode="HTML")
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from datetime import datetime
 from telegram import Update
-from telegram.ext import Application, CommandHandler, MessageHandler, filters
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackQueryHandler
 from art_buttons import start_button
-from contacts import our_contacts
+from contacts import our_contacts, how_to_use
+from sections import section_selection, button_click, contact_handler
 
 TELEGRAM_BOT_TOKEN = "7440105099:AAHnLcnOsseDfqWH_F0codMl80TN1o3gmAQ"
+
 
 def main():
     print(f"{datetime.now()} - Started")
@@ -12,6 +14,14 @@ def main():
     application.add_handler(CommandHandler("start", start_button))
     application.add_handler(
         MessageHandler(filters.TEXT & filters.Regex("^(Наши контакты ☎️)$"), our_contacts))
+    application.add_handler(
+        MessageHandler(filters.TEXT & filters.Regex("^(Как пользоваться ботом 📖)$"), how_to_use))
+    application.add_handler(
+        MessageHandler(filters.TEXT & filters.Regex("^(Записаться на секцию ✒️)$"), section_selection))
+
+    application.add_handler(CallbackQueryHandler(button_click))
+
+    application.add_handler(MessageHandler(filters.CONTACT, contact_handler))
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
